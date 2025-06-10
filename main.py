57d49cfd77affcf7ac1847ac162d5dac8b3b0b0d
kwame-Owusu <alfredowusu595@gmail.com>
2025-06-10 16:05:04 +0100

    lsp was complaining but code is correct so i added this comment

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ client = genai.Client(api_key=api_key)
 client_content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 response = client.models.generate_content(model="gemini-2.0-flash-001", contents=client_content)
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}, Response tokens: {response.usage_metadata.candidates_token_count}")
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}, Response tokens: {response.usage_metadata.candidates_token_count}") #type: ignore
+
 
