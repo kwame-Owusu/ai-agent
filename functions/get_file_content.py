676e8811c84d73c53ad9d2d97e3a44c262158d48
kwame-Owusu <alfredowusu595@gmail.com>
2025-06-12 13:17:03 +0100

    refctored file content function, and tested with tests.py and lorem.txt

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -2,28 +2,23 @@ import os
 
 MAX_CHARS = 10000
 
-def get_file_content(working_directory, filepath):
+def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
-    target_dir = abs_working_dir
-
-    if not filepath.startswith(target_dir):
-        return f'Error: Cannot read "{filepath}" as it is outside the permitted working directory'
-
-    if not os.path.isfile(filepath):
-        return f'Error: File not found or is not a regular file "{filepath}" '
-
-    
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
     try:
-        with open(filepath, "r+") as file:
-            if len(file.read()) > MAX_CHARS:
-                file.truncate(MAX_CHARS)
-                file.write(f"File '{file}' truncated at 10000 characters")
-            else:
-                file_content = file.read(MAX_CHARS)
-                return file_content
-
+        with open(abs_file_path, "r") as f:
+            content = f.read(MAX_CHARS)
+            if len(content) == MAX_CHARS:
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+        return content
     except Exception as e:
-        return f"Error: trouble reading file {e}"
+        return f'Error reading file "{file_path}": {e}'
 
 
 
