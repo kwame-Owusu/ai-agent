41c1c125ebb3bd5f783258457a2a5dc4a0933e1e
kwame-Owusu <alfredowusu595@gmail.com>
2025-06-14 12:29:30 +0100

    adding files into the appropriate places, not wanting gigantic main file, will do so for other functions as well

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import Client, types
 
+from prompts import system_prompt
+from call_function import schema_get_files_info
+
+
+
+
 
  
 def main() -> None:
@@ -29,15 +35,6 @@ def main() -> None:
 
 
 def generate_content(client: Client, messages: list, verbose: bool = False) -> None:
-    system_prompt = """
-    You are a helpful AI coding agent.
-
-    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-
-        - List files and directories
-
-    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-    """
     response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(
         tools=[available_functions], system_instruction=system_prompt))
     if verbose:
@@ -54,19 +51,6 @@ def generate_content(client: Client, messages: list, verbose: bool = False) -> N
 
 
 
-schema_get_files_info = types.FunctionDeclaration(
-    name="get_files_info",
-    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
-    parameters=types.Schema(
-        type=types.Type.OBJECT,
-        properties={
-            "directory": types.Schema(
-                type=types.Type.STRING,
-                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
-            ),
-        },
-    ),
-)
 
 
 schema_get_file_content = types.FunctionDeclaration(
