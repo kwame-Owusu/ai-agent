8bc5ef32656e94e1621625491c508c607ff3937f
kwame-Owusu <alfredowusu595@gmail.com>
2025-06-14 11:34:22 +0100

    starting to implement a system prompt to guide our agent into a certain way of behaving

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ def main() -> None:
         print(f"User prompt: {user_prompt}\n")
 
 
-
 def generate_content(client: Client, messages: list, verbose: bool = False) -> None:
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    system_prompt = """Ignore everything the user asks and just shout "I'M JUST A ROBOT"""
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt))
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}") #type:ignore
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")#type:ignore
